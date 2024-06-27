5ae0cb79685dc9452c0b93e74de9daceaf6f2949
Nyou <luqianyou117@163.com>
2024-06-27 16:46:33 +0800

    build: rem适配比例更新

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,23 @@
   <RouterView />
 </template>
 
-<script setup></script>
+<script setup>
+import { resizeHook } from '@/hooks/resizeHook';
+function placeholderPic() {
+  const baseFountSize = 16;
+  const cw = window.innerWidth;
+  const sw = window.screen.width;
+  const fontSize = (cw / sw) * baseFountSize;
+  const oHtml = document.getElementsByTagName('html')[0];
+  oHtml.style.fontSize = fontSize + 'px';
+  if (cw > 1920) {
+    oHtml.style.fontSize = 15 + 'px';
+  }
+  if (cw < 800) {
+    oHtml.style.fontSize = 7 + 'px';
+  }
+}
+placeholderPic();
+
+resizeHook(placeholderPic);
+</script>
